e4482b924e221d3de1c023559a202092ba205908
Suraj Kumar <surajkumarjupiter@gmail.com>
2022-07-27 14:51:27 +0530

    Initial commit

---
+++ src/com/suraj/Main.java
@@ -0,0 +1,27 @@
+package com.suraj;
+
+public class Main {
+    public static void main(String[] args){
+        message();
+    }
+    static void message(){
+        System.out.println("Hello World");
+        message1();
+
+    }
+    static void message1(){
+        System.out.println("Hello World");
+        message2();
+    }
+    static void message2(){
+        System.out.println("Hello World");
+        message3();
+    }
+    static void message3(){
+        System.out.println("Hello World");
+        message4();
+    }
+    static void message4(){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/com/suraj/Number.java
@@ -0,0 +1,7 @@
+package com.suraj;
+
+public class Number {
+    public static void main(String[] args){
+        System.out.println(2);
+    }
+}
